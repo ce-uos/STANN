001b6460c1ea4032cb34a4d9886b8d06245bf7cc
liy <92-liy@users.noreply.gitlabti.informatik.uni-osnabrueck.de>
2025-03-21 10:20:24 +0000

    Update padding.hpp

--- padding.hpp
+++ padding.hpp
@@ -22,7 +22,7 @@ namespace Padding{
  * @param[in]    paddValue  value to add as padding
  */
 template<int INPUT_WI, int INPUT_HI, int INPUT_BATCH_SIZE, int PAD, typename T>
-void padding(const T *input, T *output, const T paddValue){
+void padding2d(const T *input, T *output, const T paddValue){
     int OUTPUT_WI = INPUT_WI + 2 * PAD;
     int OUTPUT_HI = INPUT_HI + 2 * PAD;
 
@@ -70,6 +70,56 @@ void padding(const T *input, T *output, const T paddValue){
     }
 }
 
+
+
+template<int INPUT_CHANNELS, int INPUT_WIDTH, int OUTPUT_WIDTH, int PAD, typename T>
+void padding1d(hls::stream<T> &input, hls::stream<T> &output, T paddValue){
+
+	T input_buffer[INPUT_WIDTH * INPUT_CHANNELS];
+	T output_buffer[OUTPUT_WIDTH * INPUT_CHANNELS];
+
+	StreamUtil::toarray<INPUT_WIDTH * INPUT_CHANNELS, T>(input, input_buffer, 1);
+
+	for(int i = 0; i < INPUT_CHANNELS; i++){
+#pragma HLS PIPELINE II=1
+		for(int j = 0; j < OUTPUT_WIDTH; j++){
+	#pragma HLS UNROLL
+			if(j < PAD){
+				output_buffer[j + i * OUTPUT_WIDTH] = paddValue;
+			}else if (j >= INPUT_WIDTH + PAD){
+				output_buffer[j + i * OUTPUT_WIDTH] = paddValue;
+			}else{
+				output_buffer[j + i * OUTPUT_WIDTH] = input_buffer[j - PAD + i * INPUT_WIDTH];
+			}
+		}
+	}
+
+	StreamUtil::tostream<OUTPUT_WIDTH * INPUT_CHANNELS, T>(output_buffer, output, 1);
+}
+
+
+
+template<int INPUT_CHANNELS, int INPUT_WIDTH, int OUTPUT_WIDTH, int PAD, typename T>
+void padding1d_fast(hls::stream<T> &input, hls::stream<T> &output, T paddValue){
+
+	T input_buffer[INPUT_WIDTH];
+	T output_buffer[OUTPUT_WIDTH];
+
+		StreamUtil::toarray<INPUT_WIDTH, T>(input, input_buffer, 1);
+
+		for (int k = 0; k < PAD; k++){
+	#pragma HLS UNROLL
+			output_buffer[k] = paddValue;
+			output_buffer[k + INPUT_WIDTH] = paddValue;
+		}
+
+		for(int j = 0; j < INPUT_WIDTH; j++){
+	#pragma HLS UNROLL
+			output_buffer[PAD + j] = input_buffer[j];
+		}
+
+		StreamUtil::tostream<OUTPUT_WIDTH, T>(output_buffer, output, 1);
+	}
 } // namespace Padding
 
 #endif
